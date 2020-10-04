d31a92c67825fbb4c11ff77880c968e014f27b3e
Chandu <chandu@chandus-mbp.myfiosgateway.com>
2020-10-03 20:34:29 -0400

    final commit

--- solution.py
+++ solution.py
@@ -33,7 +33,7 @@ def webServer(port=13331):
         except IOError:
             #Send response message for file not found (404)
 
-            connectionSocket.send(bytes("HTTP/1.1 400 Not Found \r\n\r\n", "UTF-8"))
+            connectionSocket.send(bytes("HTTP/1.1 404 Not Found \r\n\r\n", "UTF-8"))
             connectionSocket.send(bytes("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n", "UTF-8"))
 
 
